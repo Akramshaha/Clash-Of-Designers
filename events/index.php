dcb699a5bc4ee2bb437646b7fcfaaf0961186d6c
mohitugemuge <mohitugemuge322@gmail.com>
2021-04-10 18:08:44 +0530

    user index header

--- events/index.php
+++ events/index.php
@@ -26,22 +26,33 @@
     <title>Document</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.3/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+    <link rel="stylesheet" href="../assets/styles/header.css">
 </head>
 
 <body>
 
-    <h1 class="text-center my-5"> Web Designing Competition System</h1>
+    <!-- header -->
+    <?php  include('header.php') ?>
+
+    <!-- content -->
 
     <div class="row m-5" id="running-events">
-        <div class="col-sm-12 pl-0">
-            <h2> Currently Running </h2>
-            <br>
-        </div>
-        <?php 
+        <div class="col-sm-8 row ">
+
+            <div class="col-sm-12 pl-4">
+                <h2> Currently Running </h2>
+            </div>
+
+            <!-- currently running events cards -->
+
+            <?php 
             $eventQuery = "SELECT id, name, description, start_time, end_time, image, registration, image, registration_starts, registration_ends FROM design_event";
             $eventResult = mysqli_query($db, $eventQuery);
             $i = 0;
 
+            ?>
+
+            <?php 
             while ( $eventRow = mysqli_fetch_assoc($eventResult)) {
                 $eventId = $eventRow['id'];
                 $startTime=date_format(date_create_from_format("Y-m-d H:i:s", $eventRow['start_time']), "Y-m-d H:i:s");
@@ -49,44 +60,66 @@
                 $currentTime = date("Y-m-d H:i:s");
 
                 if($currentTime >= $startTime && $currentTime < $endTime) { ?>
-                    <div class="col-sm-3">
-                        <div class="card" style="width: 18rem;">
-
+            <div class="col-sm-4">
+                <div class="card border-0" style="width: 100%;">
+                    <div class="p-3" style="width:18rem;">
                         <img class="card-img-top" src="<?php echo $eventRow['image'] ?>" alt="Card image cap">
-<div class="card-body">
-    <h4 class="card-title"> <?php echo $eventRow['name'] ?></h4>
-    <p class="card-text">
-        Event Date : 
-            <span class="badge badge-pill badge-secondary">
-                <?php echo date_format(date_create_from_format("Y-m-d H:i:s",$eventRow['start_time']), "d M Y") ?> 
-            </span> 
-            <br>
-        Start Time :
-            <span class="badge badge-pill badge-secondary">
-                <?php echo date_format(date_create_from_format("Y-m-d H:i:s",$eventRow['start_time']), "H:i:s A") ?> 
-            </span> 
-            <br>
-    </p>
-</div>
-<div class="card-footer p-0">
-    <a href="info?event=<?php echo base64_encode($eventRow['id']) ?>" class="btn btn-primary w-100 h-100"> Learn More </a>
-</div>
-</div>
+                        <div class="card-body">
+                            <h4 class="card-title"> <?php echo $eventRow['name'] ?></h4>
+                            <p class="card-text">
+                                Event Date :
+                                <span class="badge badge-pill badge-secondary">
+                                    <?php echo date_format(date_create_from_format("Y-m-d H:i:s",$eventRow['start_time']), "d M Y") ?>
+                                </span>
+                                <br>
+                                Start Time :
+                                <span class="badge badge-pill badge-secondary">
+                                    <?php echo date_format(date_create_from_format("Y-m-d H:i:s",$eventRow['start_time']), "H:i:s A") ?>
+                                </span>
+                                <br>
+                            </p>
+                        </div>
+                        <div class="card-footer p-0">
+                            <a href="info?event=<?php echo base64_encode($eventRow['id']) ?>"
+                                class="btn btn-primary w-100 h-100"> Learn More </a>
                         </div>
                     </div>
-    <?php      
-                $i++;
-                }  
-            }
-
-            if ( $i==0) { ?>
-             <div class="jumbotron jumbotron">
-                <div class="container">
-                    <h4 class="display-6 ml-0">No Events Currently Running</h4>
-                    <p class="lead">We are sorry to inform you that no events are currently running... Check out our upcoming events!</p>
                 </div>
             </div>
-        <?php }
+
+            <?php    
+            $i++;
+                }  
+            }  
+            ?>
+        </div>
+
+        <!-- Results -->
+        <div class="col-sm-4">
+            <h2>Results</h2>
+            <div class="bg-secondary" style="height:30vh; width:100%;">
+
+            </div>
+        </div>
+    </div>
+
+
+    </div>
+    <?php      
+             
+
+            if ( $i==0) { ?>
+
+
+
+    <div class="jumbotron jumbotron">
+        <div class="container">
+            <h4 class="display-6 ml-0">No Events Currently Running</h4>
+            <p class="lead">We are sorry to inform you that no events are currently running... Check out our upcoming
+                events!</p>
+        </div>
+    </div>
+    <?php }
     ?>
     </div>
 
@@ -106,30 +139,31 @@
             $currentTime = date("Y-m-d H:i:s");
 
             if($currentTime < $startTime ) { ?>
-                <div class="col-sm-3">
-                    <div class="card" style="width: 18rem;">
+        <div class="col-sm-3">
+            <div class="card" style="width: 18rem;">
 
-                        <img class="card-img-top" src="<?php echo $eventRow['image'] ?>" alt="Card image cap">
-                        <div class="card-body">
-                            <h4 class="card-title"> <?php echo $eventRow['name'] ?></h4>
-                            <p class="card-text">
-                                Event Date : 
-                                    <span class="badge badge-pill badge-secondary">
-                                        <?php echo date_format(date_create_from_format("Y-m-d H:i:s",$eventRow['start_time']), "d M Y") ?> 
-                                    </span> 
-                                    <br>
-                                Start Time :
-                                    <span class="badge badge-pill badge-secondary">
-                                        <?php echo date_format(date_create_from_format("Y-m-d H:i:s",$eventRow['start_time']), "H:i:s A") ?> 
-                                    </span> 
-                                    <br>
-                            </p>
-                        </div>
-                        <div class="card-footer p-0">
-                            <a href="info?event=<?php echo base64_encode($eventRow['id']) ?>" class="btn btn-primary w-100 h-100"> Learn More </a>
-                        </div>
-                    </div>
+                <img class="card-img-top" src="<?php echo $eventRow['image'] ?>" alt="Card image cap">
+                <div class="card-body">
+                    <h4 class="card-title"> <?php echo $eventRow['name'] ?></h4>
+                    <p class="card-text">
+                        Event Date :
+                        <span class="badge badge-pill badge-secondary">
+                            <?php echo date_format(date_create_from_format("Y-m-d H:i:s",$eventRow['start_time']), "d M Y") ?>
+                        </span>
+                        <br>
+                        Start Time :
+                        <span class="badge badge-pill badge-secondary">
+                            <?php echo date_format(date_create_from_format("Y-m-d H:i:s",$eventRow['start_time']), "H:i:s A") ?>
+                        </span>
+                        <br>
+                    </p>
                 </div>
+                <div class="card-footer p-0">
+                    <a href="info?event=<?php echo base64_encode($eventRow['id']) ?>"
+                        class="btn btn-primary w-100 h-100"> Learn More </a>
+                </div>
+            </div>
+        </div>
         <?php   }  
         } ?>
     </div>
